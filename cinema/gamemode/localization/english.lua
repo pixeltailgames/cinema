283f40da9be8beba76aed1f1e560608fa53c2557
samuelmaddock <samuel.maddock@gmail.com>
2013-06-10 17:03:03 -0400

    Added localization module and english strings

---
+++ cinema/gamemode/localization/english.lua
@@ -0,0 +1,41 @@
+-- Basic information
+LANG.Name		= "English"	-- Native name for language
+LANG.Id			= "en"		-- Find corresponding ID in garrysmod/resource/localization
+LANG.Author		= "Sam"		-- Chain authors if necessary (e.g. "Sam, MacDGuy, Foohy")
+
+-- Warnings
+-- cl_init.lua
+LANG.Warning_Unsupported_Line1	= "The current map is unsupported by the Cinema gamemode"
+LANG.Warning_Unsupported_Line2	= "Press F1 to open the official map on workshop"
+LANG.Warning_OSX_Line1			= "Mac OS X users may experience blank screens in Cinema"
+LANG.Warning_OSX_Line2			= "Press F1 to view troubleshooting tips and to remove this message"
+
+-- Scoreboard
+LANG.Scoreboard_A = ""
+
+-- Theater controls
+-- modules/scoreboard/cl_admin.lua
+LANG.Theater_Admin			= "ADMIN"
+LANG.Theater_Owner			= "OWNER"
+LANG.Theater_Skip			= "Skip"
+LANG.Theater_Seek			= "Seek"
+LANG.Theater_Reset			= "Reset"
+LANG.Theater_ChangeName		= "Change Name"
+LANG.Theater_QueueLock		= "Toggle Queue Lock"
+
+-- Theater list
+-- modules/scoreboard/cl_theaterlist.lua
+LANG.TheaterList_NowShowing		= "NOW SHOWING"
+
+-- Scoreboard settings panel
+-- modules/scoreboard/cl_settings.lua
+LANG.Settings_Title				= "SETTINGS"
+LANG.Settings_ClickActivate		= "CLICK TO ACTIVATE YOUR MOUSE"
+LANG.Settings_VolumeLabel		= "Volume"
+LANG.Settings_VolumeTooltip		= "Use the +/- keys to increase/decrease volume."
+LANG.Settings_HDLabel			= "HD Video Playback"
+LANG.Settings_HDTooltip			= "Enable HD video playback for HD enabled videos."
+LANG.Settings_HidePlayersLabel	= "Hide Players In Theater"
+LANG.Settings_HidePlayersTooltip= "Reduce player visibility inside of theaters."
+LANG.Settings_MuteFocusLabel	= "Mute audio while alt-tabbed"
+LANG.Settings_MuteFocusTooltip	= "Mute theater volume while Garry's Mod is out-of-focus (e.g. you alt-tabbed)."
---
+++ cinema/gamemode/translations.lua
@@ -0,0 +1,41 @@
+if CLIENT then
+	
+	translations = {}
+
+	local Languages = {}
+	local DefaultId = "en"
+
+	function translations.GetLanguage()
+		return "en" -- TODO: Get user's language
+	end
+
+	function translations.Format( key, ... )
+		local value = Languages[ translations.GetLanguage() ][ key ] or
+			Languages[ DefaultId ][ key ]
+		return string.format( value, ... )
+	end
+
+	function translations.Register( id, tbl )
+		if not ( id and tbl ) then return end
+		if not Languages[id] then Languages[id] = {} end
+		for k, v in pairs( tbl ) do
+			Languages[id][k] = v
+		end
+	end
+
+	T = translations.Format
+
+end
+
+-- Load language files
+local LanguageFiles = file.Find( GM.FolderName .. "/gamemode/localization/*", "LUA" )
+for _, filename in pairs( LanguageFiles ) do
+	if SERVER then
+		AddCSLuaFile( "localization/" .. filename )		
+	else
+		LANG = {}
+		include( "localization/" .. filename )
+		translations.Register( LANG.Id, LANG )
+		LANG = nil
+	end
+end
