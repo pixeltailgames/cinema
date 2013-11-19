30b26f97d303d24988a6d66ba2f758346f2cd03d
samuelmaddock <samuel.maddock@gmail.com>
2013-11-19 06:12:59 -0500

    Added JavaScript for Viooz service

--- cinema/gamemode/modules/theater/services/sh_viooz.lua
+++ cinema/gamemode/modules/theater/services/sh_viooz.lua
@@ -4,15 +4,13 @@ SERVICE.Name 			= "Viooz"
 SERVICE.IsTimed 		= true
 SERVICE.TheaterType 	= THEATER_PRIVATE
 
+local UrlPattern = "http://viooz.co/movies/%s-.html"
 local DataPattern = "^/movies/(%d+)%-.-%.html"
 local TitlePattern = "</span><span>(.-)</span></a>"
 local DurationPattern = "Duration: <span>(.-) min</span>"
 local ThumbnailPattern = '<img height="%d-" width="%d-" title=".-" alt=".-" src="(.-)" />'
 
 function SERVICE:Match( url )
-	PrintTable(url)
-	print("Viooz Match:")
-	print(string.match( url.path, DataPattern ))
 	return string.match( url.host, "viooz.co" ) and
 		string.match( url.path, DataPattern )
 end
@@ -43,9 +41,63 @@ function SERVICE:GetVideoInfo( data, onSuccess, onFailure )
 
 	end
 
-	local url = string.format( "http://viooz.co/movies/%s-.html", data )
+	local url = string.format( UrlPattern, data )
 	self:Fetch( url, onReceive, onFailure )
 
 end
 
 theater.RegisterService( 'viooz', SERVICE )
+
+if CLIENT then
+
+	--[[
+		Viooz hooks
+		Overrides default player behavior
+	]]
+
+	local JS_AddTheaterScript = [[
+var script = document.createElement('script');
+script.src = 'http://localhost/cinema/js/theater.js';
+document.body.appendChild(script);
+]]
+
+	local JS_OnTheaterReady = [[
+function onTheaterReady() {
+	theater.setVolume(%s);
+	theater.loadVideo('%s','%s','%s');
+}
+]]
+
+	hook.Add( "PreVideoLoad", "PreVioozLoad", function( Video )
+
+		-- Video must be Viooz
+		if Video:Type() ~= "viooz" then
+			return true
+		end
+
+		local VioozUrl = string.format( UrlPattern, Video:Data() )
+
+		local panel = theater.ActivePanel()
+		panel:Stop()
+		panel:OpenURL( VioozUrl )
+
+		local startTime = CurTime() - Video:StartTime()
+
+		-- TODO
+		panel.OnFinishLoading = function(self)
+
+			local str = string.format( JS_OnTheaterReady,
+				theater.GetVolume(), Video:Type(),
+				string.JavascriptSafe(Video:Data()), startTime )
+			self:RunJavascript(str)
+
+			self:RunJavascript(JS_AddTheaterScript)
+		end
+
+		hook.Run( "PostVideoLoad", Video )
+
+		return false
+
+	end )
+
+end
