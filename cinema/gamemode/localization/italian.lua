31f2d0df28eafb4bba63b679b7c0750535aa159c
ncristiansc <forzacristian@hotmail.it>
2014-09-04 15:08:55 +0200

    Updated italian.lua
    There are some gramatical errors that i fixed. I've changed something to make the translation more reasonable.

--- cinema/gamemode/localization/italian.lua
+++ cinema/gamemode/localization/italian.lua
@@ -7,9 +7,9 @@ LANG.Author	= "Wolfaloo"	-- Chain authors if necessary (e.g. "Sam, MacDGuy, Fooh
 LANG.Cinema	= "CINEMA"
 LANG.Volume	= "Volume"
 LANG.Voteskips	= "Vota per saltare"
-LANG.Loading	= "CARICAMENTO..."
+LANG.Loading	= "Caricamento..."
 LANG.Invalid	= "[NON VALIDO]"
-LANG.NoVideoPlaying	= "NESSUN VIDEO IN RIPRODUZIONE"
+LANG.NoVideoPlaying	= "Nessun video in riproduzione"
 LANG.Cancel	= "Cancella"
 LANG.Set	= "Setta"
 
@@ -17,7 +17,7 @@ LANG.Set	= "Setta"
 -- modules/theater/cl_init.lua
 -- modules/theater/sh_commands.lua
 -- modules/theater/sh_theater.lua
-LANG.Theater_VideoRequestedBy = C("Video corrente richiesto da ",ColHighlight,"%s",ColDefault,".")
+LANG.Theater_VideoRequestedBy = C("Video attuale richiesto da ",ColHighlight,"%s",ColDefault,".")
 LANG.Theater_InvalidRequest = "Richiesta non valida."
 LANG.Theater_AlreadyQueued = "Il video richiesto è gia in coda."
 LANG.Theater_ProcessingRequest = C("Richiedendo il video a ",ColHighlight,"%s",ColDefault," ...")
@@ -27,20 +27,20 @@ LANG.Theater_ForceSkipped = C(ColHighlight,"%s",ColDefault," ha forzato il salto
 LANG.Theater_PlayerReset = C(ColHighlight,"%s",ColDefault," ha resettato il teatro.")
 LANG.Theater_LostOwnership = "Hai perso la proprietà del teatro perchè lo hai abbandonato."
 LANG.Theater_NotifyOwnership = "Sei il padrone di questo teatro."
-LANG.Theater_OwnerLockedQueue = "Il propietario del teatro ha bloccato la fila di riproduzione."
-LANG.Theater_LockedQueue = C(ColHighlight,"%s",ColDefault," ha bloccato la fila di riproduzione.")
-LANG.Theater_UnlockedQueue = C(ColHighlight,"%s",ColDefault," ha sbloccato la fila di riproduzione.")
-LANG.Theater_OwnerUseOnly = "Solo il propietario del teatro puo usare questa funzione."
-LANG.Theater_PublicVideoLength = "Le richieste in un teatro publico sono limitate a una durata di %s second(s) secondi."
+LANG.Theater_OwnerLockedQueue = "Il proprietario del teatro ha bloccato la coda di riproduzione."
+LANG.Theater_LockedQueue = C(ColHighlight,"%s",ColDefault," ha bloccato la coda di riproduzione.")
+LANG.Theater_UnlockedQueue = C(ColHighlight,"%s",ColDefault," ha sbloccato la coda di riproduzione.")
+LANG.Theater_OwnerUseOnly = "Solo il proprietario del teatro può usare questa funzione."
+LANG.Theater_PublicVideoLength = "Le richieste in un teatro publico sono limitate ad una durata di %s second(s) secondi."
 LANG.Theater_PlayerVoteSkipped = C(ColHighlight,"%s",ColDefault," ha votato per saltare ",ColHighlight,"(%s/%s)",ColDefault,".")
 LANG.Theater_VideoAddedToQueue = C(ColHighlight,"%s",ColDefault," è stato aggiunto alla coda.")
 
 -- Warnings
 -- cl_init.lua
-LANG.Warning_Unsupported_Line1	= "La corrente mappa non è supportata da cinema gamemode"
-LANG.Warning_Unsupported_Line2	= "Premi f1 per aprire la mappa ufficiale nel Workshop steam"
-LANG.Warning_OSX_Line1	= "Utenti mac possono incontrare schermi bianchi all interno del cinema"
-LANG.Warning_OSX_Line2	= "Premi f1 per visionare i consigli e rimuovere questo messaggio"
+LANG.Warning_Unsupported_Line1	= "La mappa attuale non è supportata da cinema gamemode"
+LANG.Warning_Unsupported_Line2	= "Premi F1 per aprire la mappa ufficiale nel Workshop Steam"
+LANG.Warning_OSX_Line1	= "Gli utenti Mac possono incontrare schermi bianchi all'interno del cinema"
+LANG.Warning_OSX_Line2	= "Premi F1 per visionare i consigli e rimuovere questo messaggio"
 
 -- Queue
 -- modules/scoreboard/cl_queue.lua
@@ -53,7 +53,7 @@ LANG.Refresh_Theater	= "Ricarica teatro"
 -- Theater controls
 -- modules/scoreboard/cl_admin.lua
 LANG.Theater_Admin	= "AMMINISTRATORE"
-LANG.Theater_Owner	= "PROPIETARIO"
+LANG.Theater_Owner	= "PROPRIETARIO"
 LANG.Theater_Skip	= "Salta"
 LANG.Theater_Seek	= "Ripeti"
 LANG.Theater_Reset	= "Resetta"
@@ -88,9 +88,9 @@ LANG.Settings_MuteFocusLabel	= "Silenzia l'audio alla pressione di alt-tab"
 LANG.Settings_MuteFocusTooltip	= "Silenzia Gmod quando il gioco non è la finestra principale (es. premendo alt-tab)."
 
 -- Video Services
-LANG.Service_EmbedDisabled = "Il video richiesto non è abilitato all incorporazione."
-LANG.Service_PurchasableContent = "Il video richiesto è acquistabile e non puo essere riprodotto."
-LANG.Service_StreamOffline = "Lo stream richiesto è correntemente offline."
+LANG.Service_EmbedDisabled = "Il video richiesto non è abilitato all'incorporazione."
+LANG.Service_PurchasableContent = "Il video richiesto è acquistabile e non può essere riprodotto."
+LANG.Service_StreamOffline = "Lo stream richiesto è attualmente offline."
 
 -- Version 1.1
 LANG.TranslationsCredit = "Traduzioni di %s"
