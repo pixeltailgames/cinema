1ab0bac22ebb9e79b939b13ff62039f53c6082a2
Yuki Kaname <winterphoenix96@hotmail.com>
2015-08-15 21:32:35 -0400

    Optimize Location System (1/2)

--- cinema/gamemode/modules/location/sv_init.lua
+++ cinema/gamemode/modules/location/sv_init.lua
@@ -3,18 +3,67 @@ module( "Location", package.seeall )
 -- Prevent sending clients a theater video too early
 local DelayThinkSeconds = 3
 
-hook.Add( "PlayerThink", "LocationThink", function( ply )
-	if not ply:IsConnected() or (ply:TimeConnected() < DelayThinkSeconds) then return end
+function Initialize()
+	if LocBrushes != {} then
+		for brushKey, brush in pairs(LocBrushes) do
+			if IsValid(brush) then -- This refuses to be true, for some reason...
+				brush:Remove()
+			end
+			brush = nil
+			LocBrushes[brushKey] = nil
+		end
+	end
+	for locName, loc in pairs(GetLocations()) do
+		local brush = ents.Create("base_brush")
+		brush:Spawn()
+		brush:SetSolid(SOLID_BBOX)
+		brush:SetCollisionBoundsWS(loc.Min, loc.Max)
+		brush:SetTrigger(true)
+		brush.locIndex = loc.Index
+		function brush:EndTouch(ply)
+			if !ply:IsPlayer() then return end
+			while IsValid(ply) do
+				repeat
+					if RealTime() - ply.LocationChangeTime > 0.25 then -- Make sure they didn't _just_ change location
+						ply:SetLocation(0) -- Set to Unknown after exiting a Brush Area in case they're not entering another one
+					end
+					
+					break
+				until ply:IsConnected() and (ply:TimeConnected() >= DelayThinkSeconds)
+				break
+			end
+		end
+		function brush:StartTouch(ply)
+			if !ply:IsPlayer() then return end
+			while IsValid(ply) do
+				repeat
+					local oldloc = ply:GetLocation()
+					local loc = self.locIndex
+					if oldloc == loc then return end
+					
+					ply:SetLocation(loc)
+					ply.LocationChangeTime = RealTime()
+					
+					-- Player, New Location, Old Location
+					hook.Call( "PlayerChangeLocation", GAMEMODE, ply, loc, oldloc )
+					break
+				until ply:IsConnected() and (ply:TimeConnected() >= DelayThinkSeconds)
+				break
+			end
+		end
+		table.insert(LocBrushes, brush)
+	end
+end
 
-	local oldloc = ply:GetLocation()
-	local loc = Find( ply )
-	if oldloc == loc then return end
-	
-	ply:SetLocation(loc)
+hook.Add("OnReloaded", "ReinitializeLocationBrushes", function()
+	timer.Simple(0.5, function()
+		Initialize()
+	end)
+end)
 
-	-- Player, New Location, Old Location
-	hook.Call( "PlayerChangeLocation", GAMEMODE, ply, loc, oldloc )
-end )
+hook.Add("InitPostEntity", "InitializeLocationBrushes", function()
+	Initialize()
+end)
 
 -- Lua refresh theater fix (used in development)
 hook.Add("OnReloaded", "ResetPlayerLocations", function()
