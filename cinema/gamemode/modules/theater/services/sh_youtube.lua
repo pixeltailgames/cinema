de6e707947d1a7fba9eb82bd95a70d15d6127b80
Shadowsun™ <FarukGamer@users.noreply.github.com>
2020-08-23 03:04:46 +0200

    Revert "Fix YouTube"
    This reverts commit 70eb619430e9d7b8d842d0300310c2ff593eb6b8.

--- cinema/gamemode/modules/theater/services/sh_youtube.lua
+++ cinema/gamemode/modules/theater/services/sh_youtube.lua
@@ -1,14 +1,14 @@
---[[
-Credits to veitikka (https://github.com/veitikka) for fixing YouTube service and writing the
-Workaround with a Metadata parser.
---]]
-
 local SERVICE = {}
 
 SERVICE.Name 	= "YouTube"
 SERVICE.IsTimed = true
 
-local METADATA_URL = "https://www.youtube.com/watch?v=%s"
+local API_KEY = "AIzaSyAjSwUHzyoxhfQZmiSqoIBQpawm2ucF11E"
+
+local METADATA_URL = "https://www.googleapis.com/youtube/v3/videos?id=%s" ..
+		"&key=" .. API_KEY ..
+		"&part=snippet,contentDetails,status" ..
+		"&videoEmbeddable=true&videoSyndicated=true"
 
 function SERVICE:Match( url )
 	return string.match( url.host, "youtu.?be[.com]?" )
@@ -59,43 +59,6 @@ local function convertISO8601Time( duration )
 	return duration
 end
 
----
--- Get the value for an attribute from a html element
---
-local function ParseElementAttribute( element, attribute )
-	if not element then return end
-	-- Find the desired attribute
-	local output = string.match( element, attribute.."%s-=%s-%b\"\"" )
-	if not output then return end
-	-- Remove the 'attribute=' part
-	output = string.gsub( output, attribute.."%s-=%s-", "" )
-	-- Trim the quotes around the value string
-	return string.sub( output, 2, -2 )
-end
-
----
--- Get the contents of a html element by removing tags
--- Used as fallback for when title cannot be found
---
-local function ParseElementContent( element )
-	if not element then return end
-	-- Trim start
-	local output = string.gsub( element, "^%s-<%w->%s-", "" )
-	-- Trim end
-	return string.gsub( output, "%s-</%w->%s-$", "" )
-end
-
--- Lua search patterns to find metadata from the html
-local patterns = {
-	["title"] = "<meta%sproperty=\"og:title\"%s-content=%b\"\">",
-	["title_fallback"] = "<title>.-</title>",
-	["thumb"] = "<meta%sproperty=\"og:image\"%s-content=%b\"\">",
-	["thumb_fallback"] = "<link%sitemprop=\"thumbnailUrl\"%s-href=%b\"\">",
-	["duration"] = "<meta%sitemprop%s-=%s-\"duration\"%s-content%s-=%s-%b\"\">",
-	["live"] = "<meta%sitemprop%s-=%s-\"isLiveBroadcast\"%s-content%s-=%s-%b\"\">",
-	["live_enddate"] = "<meta%sitemprop%s-=%s-\"endDate\"%s-content%s-=%s-%b\"\">"
-}
-
 function SERVICE:GetURLInfo( url )
 
 	local info = {}
@@ -109,7 +72,7 @@ function SERVICE:GetURLInfo( url )
 		info.Data = string.match(url.path, "^/v/([%a%d-_]+)")
 
 	-- http://youtu.be/(videoId)
-	elseif string.match(url.host, "youtu.be") and
+	elseif string.match(url.host, "youtu.be") and 
 		url.path and string.match(url.path, "^/([%a%d-_]+)$") and
 		( !info.query or #info.query == 0 ) then -- short url
 		info.Data = string.match(url.path, "^/([%a%d-_]+)$")
@@ -132,7 +95,7 @@ function SERVICE:GetURLInfo( url )
 			time = tonumber(time) and time or 0
 			time = time + (minutes * 60)
 		end
-
+		
 		if hours then
 			time = tonumber(time) and time or 0
 			time = time + (hours * 60 * 60)
@@ -152,59 +115,42 @@ function SERVICE:GetURLInfo( url )
 
 end
 
----
--- Function to parse video metadata straight from the html instead of using the API
---
-function SERVICE:ParseYTMetaDataFromHTML( html )
-	--MetaData table to return when we're done
-	local metadata = {}
-
-	-- Fetch title and thumbnail, with fallbacks if needed
-	metadata.title = ParseElementAttribute(string.match(html, patterns["title"]), "content")
-		or ParseElementContent(string.match(html, patterns["title_fallback"]))
-
-	-- Parse HTML entities in the title into symbols
-	metadata.title = url.htmlentities_decode(metadata.title)
-
-	metadata.thumbnail = ParseElementAttribute(string.match(html, patterns["thumb"]), "content")
-		or ParseElementAttribute(string.match(html, patterns["thumb_fallback"]), "href")
-
-	-- See if the video is an ongoing live broadcast
-	-- Set duration to 0 if it is, otherwise use the actual duration
-	local isLiveBroadcast = tobool(ParseElementAttribute(string.match(html, patterns["live"]), "content"))
-	local broadcastEndDate = string.match(html, patterns["live_enddate"])
-	if isLiveBroadcast and not broadcastEndDate then
-		-- Mark as live video
-		metadata.duration = 0
-	else
-		local durationISO8601 = ParseElementAttribute(string.match(html, patterns["duration"]), "content")
-		if isstring(durationISO8601) then
-			metadata.duration = math.max(1, convertISO8601Time(durationISO8601))
-		end
-	end
-
-	return metadata
-end
-
 function SERVICE:GetVideoInfo( data, onSuccess, onFailure )
 
 	local onReceive = function( body, length, headers, code )
-		local status, metadata = pcall(self.ParseYTMetaDataFromHTML, self, body)
-		if not status  then
+		local resp = util.JSONToTable( body )
+		if not resp then
 			return onFailure( 'Theater_RequestFailed' )
 		end
 
-		local info = {}
-		info.title = metadata["title"]
-		info.thumbnail = metadata["thumbnail"]
+		if resp.error then
+			return onFailure( 'Theater_RequestFailed' )
+		end
+		
+		if table.Lookup( resp, 'pageInfo.totalResults', 0 ) <= 0 then
+			return onFailure( 'Theater_RequestFailed' )
+		end
+		
+		local item = resp.items[1]
 
-		local isLive = metadata["duration"] == 0
+		if not table.Lookup( item, 'status.embeddable' ) then
+			return onFailure( 'Service_EmbedDisabled' )
+		end
+		
+		local info = {}
+		info.title = table.Lookup( item, 'snippet.title' )
+
+		-- Medium Size doesn't have a letterbox
+		info.thumbnail = table.Lookup( item, 'snippet.thumbnails.medium.url' )
+
+		local isLive = ( table.Lookup( item, 'snippet.liveBroadcastContent' ) ~= 'none' )
 
 		if isLive then
 			info.type = 'youtubelive'
 			info.duration = 0
 		else
-			info.duration = metadata["duration"]
+			local durStr = table.Lookup( item, 'contentDetails.duration', '' )
+			info.duration = convertISO8601Time( durStr )
 		end
 
 		if onSuccess then
